3963e0b88fae55dd8840cafec1d6858dc0d4365a
Konrad Lis <o.konradlis@gmail.com>
2019-10-13 17:32:28 +0200

    Before install Bavel-eslint

--- src/js/script.js
+++ src/js/script.js
@@ -90,7 +90,7 @@
       thisProduct.renderInMenu();
       //thisProduct.initAccordion();
 
-      //console.log('new Product:', thisProduct);
+      console.log('new Product:', thisProduct);
     }
     renderInMenu() {
       const thisProduct = this;
@@ -107,35 +107,35 @@
       menuContainer.appendChild(thisProduct.element);
     }
   }
-  //initAccordion() {
-  //  const thisProduct = this;
-  //  console.log('thisinitAccordion:', thisProduct);
+  //  initAccordion() {
+  //const thisProduct = this;
+  //console.log('thisinitAccordion:', thisProduct);
 
   /* find the clickable trigger (the element that should react to clicking) */
-  //const clickableTrigger = document.querySelector(select.menuProduct.clickable);
-  //console.log('clickableTrigger:', clickableTrigger);
+  //  const clickableTrigger = document.querySelector(select.menuProduct.clickable);
+  //  console.log('clickableTrigger:', clickableTrigger);
   /* START: click event listener to trigger */
   //clickableTrigger.addEventListener('click', function (event) {
-  //console.log('clicked')
+  //  console.log('clicked')
   /* prevent default action for event */
   //event.preventDefault();
   /* toggle active class on element of thisProduct */
-  //thisProduct.element.classList.toggle('active');
+  //  thisProduct.element.classList.toggle('active');
 
   /* find all active products */
-  //const activeProducts = thisProduct.querySelectorAll(select.menuProductsActive);
+  //  const activeProducts = thisProduct.querySelectorAll(select.menuProductsActive);
   //console.log('activeProducts:', activeProducts);
   /* START LOOP: for each active product */
-  //for (let activeProduct of activeProducts) {
+  //      for (let activeProduct of activeProducts) {
   /* START: if the active product isn't the element of thisProduct */
-  //if (activeProduct != thisProduct) {
+  //    if (activeProduct != thisProduct) {
   /* remove class active for the active product */
-  //thisProduct.classList.remove('active');
+  //  thisProduct.classList.remove('active');
   /* END: if the active product isn't the element of thisProduct */
   //  }
   /* END LOOP: for each active product */
   //}
   /* END: click event listener to trigger */
-
+  //}
   app.init();
 }
