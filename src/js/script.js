2b13daba9e718fe484cc5f9cfab5bad209658a7c
Konrad Lis <o.konradlis@gmail.com>
2019-10-14 22:35:43 +0200

    Start algorithm processOrder - comments

--- src/js/script.js
+++ src/js/script.js
@@ -170,7 +170,7 @@
           thisProduct.processOrder();
           //console.log('change', event);
         });
-        thisProduct.cartButton.addEventListener('click', function(event){
+        thisProduct.cartButton.addEventListener('click', function (event) {
           event.preventDefault();
           thisProduct.processOrder();
           //console.log('click', event);
@@ -180,8 +180,31 @@
     processOrder() {
       const thisProduct = this;
       //console.log('processOrder', thisProduct);
+      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData:', formData);
+      /* set variable price to equal thisProduct.data.price */
+
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      /* save the element in thisProduct.data.params with key paramId as const param */
+
+      /* START LOOP: for each optionId in param.options */
+      /* save the element in param.options with key optionId as const option */
+
+      /* START IF: if option is selected and option is not default */
+      /* add price of option to variable price */
+      /* END IF: if option is selected and option is not default */
+      /* START ELSE IF: if option is not selected and option is default */
+      /* deduct price of option from price */
+
+      /* END ELSE IF: if option is not selected and option is default */
+
+      /* END LOOP: for each optionId in param.options */
+
+      /* END LOOP: for each paramId in thisProduct.data.params */
+
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+
     }
   }
   app.init();
