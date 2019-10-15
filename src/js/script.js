9d81a5cc7ad14bf9fa9dafb6bee73e0594cf912b
Konrad Lis <o.konradlis@gmail.com>
2019-10-15 16:28:48 +0200

    Add to 'processOrder' loop for-in

--- src/js/script.js
+++ src/js/script.js
@@ -182,27 +182,37 @@
       //console.log('processOrder', thisProduct);
       /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData:', formData);
+      //console.log('formData:', formData);
       /* set variable price to equal thisProduct.data.price */
-
+      const price = thisProduct.data.price;
+      //console.log('price:', price);
       /* START LOOP: for each paramId in thisProduct.data.params */
-      /* save the element in thisProduct.data.params with key paramId as const param */
+      //const paramID =
+      for (let paramID in thisProduct.data.params) {
+        console.log('paramID:', paramID);
+        /* save the element in thisProduct.data.params with key paramId as const param */
+        const param = thisProduct.data.params[paramID];
+        console.log('param:', param);
+        /* START LOOP: for each optionId in param.options */
 
-      /* START LOOP: for each optionId in param.options */
-      /* save the element in param.options with key optionId as const option */
+        /* save the element in param.options with key optionId as const option */
 
-      /* START IF: if option is selected and option is not default */
-      /* add price of option to variable price */
-      /* END IF: if option is selected and option is not default */
-      /* START ELSE IF: if option is not selected and option is default */
-      /* deduct price of option from price */
+        /* START IF: if option is selected and option is not default */
 
-      /* END ELSE IF: if option is not selected and option is default */
+        /* add price of option to variable price */
 
-      /* END LOOP: for each optionId in param.options */
+        /* END IF: if option is selected and option is not default */
 
-      /* END LOOP: for each paramId in thisProduct.data.params */
+        /* START ELSE IF: if option is not selected and option is default */
 
+        /* deduct price of option from price */
+
+        /* END ELSE IF: if option is not selected and option is default */
+
+        /* END LOOP: for each optionId in param.options */
+
+        /* END LOOP: for each paramId in thisProduct.data.params */
+      }
       /* set the contents of thisProduct.priceElem to be the value of variable price */
 
     }
